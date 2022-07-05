f7a81dddc7f490d5a39d97538e994b785a057bd3
szt <soulzterx@googlemail.com>
2022-07-05 22:54:51 +0200

    finished first draft

--- disksleeper.sh
+++ disksleeper.sh
@@ -3,7 +3,12 @@
 DISKS_TO_SLEEP=(
     "hdd-lvraid"
 )
-#for disk in ${DISKS_TO_SLEEP[@]}; do
-#    sync
-#    hdparm -y $(lvdisplay -m /dev/mapper/${disk} | grep -oE '\/dev/sd.?' | sort -u)
-#done
+sync
+for disk in ${DISKS_TO_SLEEP[@]}; do
+    DISK_IO=$(atop -P LVM 1 5 | grep 'hdd-lvraid' | gawk 'NR!=1 {s+=$8} END {if (s>0) print s}')
+    if [[ -z "$DISK_IO" ]]; then
+        echo "Putting disk $disk to sleep"
+        sync
+        hdparm -y $(lvdisplay -m /dev/mapper/$disk | grep -oE '\/dev/sd.?' | sort -u)
+    fi
+done
