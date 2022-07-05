f3b1f0dad0cb86a02c1779eef3bf79d78cfc43a0
szt <soulzterx@googlemail.com>
2022-07-06 01:31:16 +0200

    fix

--- bin/disksleeper.sh
+++ bin/disksleeper.sh
@@ -9,8 +9,9 @@ sync
 for disk in ${DISKS_TO_SLEEP[@]}; do
     DISK_IO=$(atop -P LVM $INTERVAL $TIMES | grep 'hdd-lvraid' | gawk 'NR!=1 {s+=$8} END {if (s>0) print s}')
     if [[ -z "$DISK_IO" ]]; then
-        echo "Putting disk $disk to sleep"
+        echo "Spinning down $disk..."
         sync
-        hdparm -y $(lvdisplay -m /dev/mapper/$disk | grep -oE '\/dev/sd.?' | sort -u)
+        hdparm -y $(lvs -o seg_pe_ranges /dev/mapper/$disk | grep -oE '\/dev/sd.?' | sort -u)
+        echo "...done"
     fi
 done
