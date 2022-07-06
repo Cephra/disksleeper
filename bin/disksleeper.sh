194b3e48074420bd11a07485cd65f706a1f7f4c8
szt <soulzterx@googlemail.com>
2022-07-06 21:38:34 +0200

    checking if disks are already standby

--- bin/disksleeper.sh
+++ bin/disksleeper.sh
@@ -1,18 +1,25 @@
 #/usr/bin/env bash
 
 INTERVAL=1
-TIMES=5
-DISKS_TO_SLEEP=(
+TIMES=30
+LVS_TO_SLEEP=(
     "hdd-lvraid"
 )
 sync
-for disk in ${DISKS_TO_SLEEP[@]}; do
-    DISK_IO=$(atop -P LVM $INTERVAL $TIMES | grep 'hdd-lvraid' | gawk 'NR!=1 {s+=$8} END {if (s>0) print s}')
+for lvol in ${LVS_TO_SLEEP[@]}; do
+    DISK_IO=$(atop -P LVM $INTERVAL $TIMES | grep "$lvol" | gawk 'NR!=1 {s+=$8} END {if (s>0) print s}')
     if [[ -z "$DISK_IO" ]]; then
-        echo "Spinning down $disk..."
-        sync
-        hdparm -y $(lsblk -nlpso NAME /dev/mapper/$disk | grep -oE '^.+?sd.?[^0-9]$')
-        echo "...done"
+        LV_DISKS=$(lsblk -nlpso NAME /dev/mapper/$lvol | grep -oE '^.+?sd.?[^0-9]$')
+        for pdisk in ${LV_DISKS[@]}; do
+            DISK_SPINNING=$(hdparm -C $pdisk | grep -oE 'standby')
+            if [[ -z "$DISK_SPINNING" ]]; then
+                sync
+                hdparm -y $pdisk
+            else
+                echo $DISK_IO
+                echo 'no disks spinning'
+            fi
+        done
     fi
 done
 
